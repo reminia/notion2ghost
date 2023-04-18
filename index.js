c1077423b5eae781a6f7ba5005b29095d4743b4e
sleefd <sleefd@gmail.com>
2023-04-18 22:54:37 +0800

    complete first version

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 const { Client } = require("@notionhq/client");
 const { NotionToMarkdown } = require("notion-to-md");
 const fs = require('fs');
-// or
-// import {NotionToMarkdown} from "notion-to-md";
+
+require('dotenv').config()
+const notionKey = process.env.NotionKey
+const ghostHost = process.env.GhostHost
+const ghostKey = process.env.GhostKey
+
+const prompt = require("prompt-sync")({ sigint: true });
+const pageId = prompt("notion page id: ");
+const title = prompt("post title: ");
+const excerpt = prompt("post excerpt: ");
+const tags = prompt("post tags: ").split(",");
 
 const notion = new Client({
-  auth: "your integration token",
+  auth: notionKey,
 });
-
 // passing notion client to the option
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
-(async () => {
-  const mdblocks = await n2m.pageToMarkdown("target_page_id");
-  const mdString = n2m.toMarkdownString(mdblocks);
+const GhostAdminAPI = require('@tryghost/admin-api');
+const ghost = new GhostAdminAPI({
+  url: ghostHost, // move to config
+  key: ghostKey,
+  version: 'v4'
+});
 
-  //writing to file
-  fs.writeFile("test.md", mdString, (err) => {
+const mdoc = {"version":"0.3.1","atoms":[],"cards":[["markdown", {"cardName": "markdown"}]],"markups":[],"sections": [[10, 0]]};
+
+(async () => {
+  const mdblocks = await n2m.pageToMarkdown(pageId);
+  const mdString = n2m.toMarkdownString(mdblocks);
+  mdoc.cards[0][1]["markdown"] = mdString;
+  const json = JSON.stringify(mdoc);
+  const post = {
+    status: "published",
+    tags: tags,
+    title: title,
+    mobiledoc: json,
+    custom_excerpt: excerpt
+  };
+  ghost.posts.add(post)
+    .then((response) => {
+      //console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  //writing markdown to file
+  fs.writeFile("post.md", mdString, (err) => {
     console.log(err);
   });
 })();
